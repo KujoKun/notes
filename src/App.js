8f4b145c88de2231e4e1230afc73492a7f5739cf
kujo-kun <kazuya.kujo2@gmail.com>
2022-04-17 06:21:52 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   const [notes, setNotes] = useState([
     {
       id: nanoid(),
-      text: "This is a small application used to create small notes!",
+      text: "OBAMNA LULW OBAMNA LULW OBAMNA LULW OBAMNA LULW",
       date: "12-04-2022",
     },
     {
